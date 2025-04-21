5486550d515403b78cb8bb9d46e4d228eb112cba
Utrechtsedev <aichou@innovatieweb.nl>
2025-04-21 15:54:24 +0200

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { siteTitle } from '$lib/config';
+
+	let { data } = $props();
+</script>
+
+<svelte:head>
+	<title>{siteTitle}</title>
+</svelte:head>
+
+<!-- This is the README.md file in the root of the repo. It serves double duty as the homepage's content. If you'd rather use your own HTML and/or Svelte, you can delete/modify everything in this file. -->
+<data.ReadMe />
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from '@tailwindcss/vite';
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit(), tailwindcss()],
+  server: {
+    fs: {
+      allow: ["."],
+    },
+  },
+};
+
+export default config;
