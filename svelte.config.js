269b63fefefd4badf100f9b934c3591f33909dfc
Utrechtsedev <moughit53@gmail.com>
2025-04-23 00:02:10 +0200

    changed static for node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-node";
 import { mdsvex } from "mdsvex";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
