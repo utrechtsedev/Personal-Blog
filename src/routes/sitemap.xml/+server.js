712082373936ce019febf206c095a16f6de0fa9c
Utrechtsedev <moughit53@gmail.com>
2025-04-28 21:49:09 +0200

    changed mistake in sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -3,6 +3,6 @@ import * as sitemap from 'super-sitemap';
 export const prerender = true;
 export const GET = async () => {
   return await sitemap.response({
-    origin: 'https://spoedspecialist.nl',
+    origin: 'https://aichou.nl',
   });
 };
